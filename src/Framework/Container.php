3ed52a27fa109d549f98fa521c8c1529754c38ff
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-05-15 12:06:57 +1200

    reflection class

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass;
+
 class Container{
     private array $definitions = [];
 
     public function addDefinitions(array $newDefinition)
     {
         $this->definitions = [...$this->definitions, ...$newDefinition];
-        dd($this->definitions);
+
+    }
+
+    public function resolve(string $classname){
+        $reflectionClass = new ReflectionClass($classname);
+
+        dd($reflectionClass);
     }
 
 }
