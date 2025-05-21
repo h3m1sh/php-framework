b88f58bfb087084539ef420c975208f33f10096f
Hamish Elliot <hamishelliot@Hamishs-MacBook-Pro.local>
2025-05-21 15:52:06 +1200

    feat: transaction page & validation rules

--- database.sql
+++ database.sql
@@ -5,8 +5,20 @@ CREATE TABLE IF NOT EXISTS users(
     age tinyint(3) unsigned NOT NULL,
     country varchar(255) NOT NULL,
     social_media_url varchar(255) NOT NULL,
-    create_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY(id),
     UNIQUE KEY(email)
+);
+
+CREATE TABLE IF NOT EXISTS transactions(
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    description varchar(255) NOT NULL,
+    amount decimal(10,2) NOT NULL,
+    date datetime NOT NULL ,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    user_id bigint unsigned NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
